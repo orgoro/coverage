67f18b082e9028420072dd2b2e8b058b780c084b
orgoro <or@d-id.com>
2021-10-12 12:21:56 +0300

    add test

--- src/coverage.test.ts
+++ src/coverage.test.ts
@@ -3,13 +3,22 @@ import fs from 'fs'
 import {parse} from 'path/posix'
 
 describe('tests', () => {
-  it('parses coverage as expected', () => {
+  it('parses coverage as expected when float', () => {
     const report = fs.readFileSync('./coverage.xml', 'utf8')
     const parsed = parseFilesCoverage(report, 'src', ['src/coverage.ts'], 0.8)
     expect(parsed).toBeDefined()
     expect(parsed![0]).toBeDefined()
     expect(parsed![0].pass).toBeTruthy()
     expect(parsed![0].cover).toBe(0.99)
+  })
+
+  it('parses coverage as expected when zero', () => {
+    const report = fs.readFileSync('./coverage.xml', 'utf8')
+    const parsed = parseFilesCoverage(report, 'src', ['src/main.ts'], 0.01)
+    expect(parsed).toBeDefined()
+    expect(parsed![0]).toBeDefined()
+    expect(parsed![0].pass).toBeFalsy()
+    expect(parsed![0].cover).toBe(0)
   })
 
   it('parses source', () => {
