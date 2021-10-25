164faa6b0d424ec449e6959d340109c8fe54d281
orgoro <or@d-id.com>
2021-10-25 09:41:12 +0300

    Feature/check (#43)

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ import readFile from './readFile'
 const checkName = 'Coverge Results'
 
 async function run(): Promise<void> {
+  let checkId = -1
+
   try {
     const coverageFile: string = core.getInput('coverageFile', {required: true})
     core.debug(`coverageFile: ${coverageFile}`)
@@ -16,21 +18,66 @@ async function run(): Promise<void> {
     const eventName = context.eventName
     if (eventName !== 'pull_request') {
       core.setFailed(`action support only pull requests but event is ${eventName}`)
-      return;
+      return
     }
     const {pull_request} = context.payload
     const base = pull_request?.base.sha
     const head = pull_request?.head.sha
 
-    const checks = await octokit.rest.checks.listForRef({
-      ...context.repo,
-      ref: head
-    })
+    checkId = await getCheck(head)
 
-    const existingCheck = checks.data?.check_runs?.find(check => check.name === checkName)
+    core.info(`comparing commits: base ${base} <> head ${head}`)
+    const files = await compareCommits(base, head)
+    core.info(`git new files: ${JSON.stringify(files.newFiles)} modified files: ${JSON.stringify(files.modifiedFiles)}`)
 
-    let checkId = -1
+    const report = readFile(coverageFile)
+    const filesCoverage = parseCoverageReport(report, files)
+    const {passOverall, message} = messagePr(filesCoverage)
 
+    const status = passOverall ? 'completed' : 'failure'
+    const conclusion = passOverall ? 'success' : 'failed'
+
+    if (checkId > 0) {
+      octokit.rest.checks.update({
+        ...context.repo,
+        check_run_id: checkId,
+        status,
+        conclusion,
+        output: {title: 'Coverage Results - ', summary: message}
+      })
+    }
+
+    if (!passOverall) {
+      core.setFailed('Coverage is lower then configured threshold 😭')
+    }
+  } catch (error) {
+    const message = JSON.stringify(error)
+    core.setFailed(message)
+    if (checkId > 0) {
+      octokit.rest.checks.update({
+        ...context.repo,
+        check_run_id: checkId,
+        status: 'failure',
+        conclusion: 'failed',
+        output: {title: 'Coverage Results Failed', summary: message}
+      })
+    }
+  }
+}
+
+run()
+
+async function getCheck(head: string): Promise<number> {
+  const checks = await octokit.rest.checks.listForRef({
+    ...context.repo,
+    ref: head
+  })
+
+  const existingCheck = checks.data?.check_runs?.find(check => check.name === checkName)
+
+  let checkId = -1
+
+  try {
     if (existingCheck) {
       checkId = existingCheck.id
       core.info(`existing checkId: ${checkId}`)
@@ -49,36 +96,9 @@ async function run(): Promise<void> {
       checkId = respond.data.id
       core.info(`new checkId: ${checkId}`)
     }
-
-    core.info(`comparing commits: base ${base} <> head ${head}`)
-    const files = await compareCommits(base, head)
-    core.info(`git new files: ${JSON.stringify(files.newFiles)} modified files: ${JSON.stringify(files.modifiedFiles)}`)
-
-    const report = readFile(coverageFile)
-    const filesCoverage = parseCoverageReport(report, files)
-    const {passOverall, message} = messagePr(filesCoverage)
-
-    if (passOverall) {
-      octokit.rest.checks.update({
-        ...context.repo,
-        check_run_id: checkId,
-        status: 'completed',
-        conclusion: 'success',
-        output: {title: 'Coverage Results ✅', summary: message}
-      })
-    } else {
-      octokit.rest.checks.update({
-        ...context.repo,
-        check_run_id: checkId,
-        status: 'failure',
-        conclusion: 'failed',
-        output: {title: 'Coverage Results ❌', summary: message}
-      })
-      core.setFailed('Coverage is lower then configured threshold 😭')
-    }
-  } catch (error) {
-    core.setFailed(JSON.stringify(error))
+  } catch (e) {
+    core.warning('could not create a check - you might be running from a fork or token has no write permission')
   }
-}
 
-run()
+  return checkId
+}
